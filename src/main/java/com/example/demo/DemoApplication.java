9e2a91d5e449887da5e89638bce694ca5040d2f5
francis <15527804816@163.com>
2018-08-27 16:19:07 +0800

    3

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
-		System.out.println("sssssss132ssss");
+		System.out.println("");
         SpringApplication.run(DemoApplication.class, args);
     }
 }
